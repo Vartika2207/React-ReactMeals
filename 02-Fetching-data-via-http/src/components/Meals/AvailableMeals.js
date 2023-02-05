fed704a819092739894fe331a8cd1d7a5f958b0a
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2023-02-05 17:26:51 +0530

    Added react scripts

---
+++ 02-Fetching-data-via-http/src/components/Meals/AvailableMeals.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import classes from './AvailableMeals.module.css';
+
+
+const AvailableMeals = () => {
+
+  const [meals, setMeals] = useState([]);
+
+  // will get triggered when app.js uploads for 1st time
+  useEffect(() => {
+
+    // created this below function bcz fetch return promise and overall useEffect can't return promise.
+    const fetchMeals = async () => {
+      const url = 'https://react-http-43b44-default-rtdb.firebaseio.com/meals.json';
+      // fetch returns a promise since sending http request is asynch task=> hence .then() or await
+      const response = await fetch(url);
+
+      // below retuns an object and array is needed
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+      for(const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price
+        });
+      }
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals();
+  }, []);
+
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
